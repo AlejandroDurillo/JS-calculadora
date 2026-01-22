d209da656f06180b666bd2552f03ed77d7400c52
AlejandroDurillo <adurvar2106@g.educaand.es>
2026-01-22 19:46:26 +0100

    Upload content

---
+++ js/JavaScript.js
@@ -0,0 +1,38 @@
+document.addEventListener(DOMContentLoaded, () =>{
+
+    const pantalla = document.getElementById("pantalla");
+    
+    const btn_dos = document.getElementById("boton_dos");
+    const btn_tres = document.getElementById("boton_tres");
+    const btn_cuatro = document.getElementById("boton_cuatro");
+    const btn_cinco = document.getElementById("boton_cinco");
+    const btn_seis= document.getElementById("boton_seis");
+    const btn_siete= document.getElementById("boton_siete");
+    const btn_ocho = document.getElementById("boton_ocho");
+    const btn_nueve = document.getElementById("boton_nueve");
+
+    
+    const btn_menos = document.getElementById("boton_menos");
+    const btn_mult = document.getElementById("boton_mult");
+    const btn_divi = document.getElementById("boton_divi");
+
+    var operacion1 = document.getElementById("numeros")
+
+    const btn_uno = document.getElementById("boton_uno");
+
+    if(btn_uno){
+        btn_uno.addEventListener("click", () => {
+            const texto = document.getElementById("");
+            texto.textContent = "1";
+            console.log("1");
+        });
+}
+                    
+
+
+
+
+
+
+
+});
